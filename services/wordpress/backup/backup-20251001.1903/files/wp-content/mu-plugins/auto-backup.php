a2b3cf5e690be009f0033c447e1d904d87929045
Newfile01 <nicolas.morel.01000@gmail.com>
2025-10-06 16:54:53 +0200

    Suppression des données de backup

--- services/wordpress/backup/backup-20251001.1903/files/wp-content/mu-plugins/auto-backup.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Plugin Name: Auto Backup on Update
- * Description: Déclenche un backup après chaque mise à jour WordPress.
- */
-
-add_action('upgrader_process_complete', function() {
-    shell_exec('/bin/backup_and_push.sh > /tmp/backup.log 2>&1 &');
-}, 10, 2);
